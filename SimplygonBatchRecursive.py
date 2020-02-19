0d7d0e7a624edafed8a2d1fbfa9810e84bd0f4da
Dumble009 <daichi19990821@gmail.com>
2020-02-19 20:16:45 +0900

    Support obj

--- SimplygonBatchRecursive.py
+++ SimplygonBatchRecursive.py
@@ -1,6 +1,7 @@
 from subprocess import Popen
 import glob
 import sys
+import os
 
 if __name__ == "__main__":
     args = sys.argv
@@ -10,10 +11,12 @@ if __name__ == "__main__":
     rulePath = args[3]
     pathOfSimplygonBatch = "C:\\PROGRA~1\\Simplygon\\8\\Tools\\SimplygonBatch\\SimplygonBatch.exe"
 
-    fbxPaths = glob.glob(inputRootPath + "/**/*.fbx", recursive = True)
-    for inputPath in fbxPaths:
+    modelPaths = []
+    #modelPaths = glob.glob(inputRootPath + "/**/*.fbx", recursive = True)
+    modelPaths.extend(glob.glob(inputRootPath + "/**/*.obj", recursive = True))
+    for inputPath in modelPaths:
 
-        command = pathOfSimplygonBatch + " --Input " + inputPath + " --Output " + outputPath + " --Spl " + rulePath + " --OutputFileFormat .fbx --Verbose" + '\"'
+        command = pathOfSimplygonBatch + " --Input \"" + inputPath + "\" --Output \"" + outputPath + "\" --Spl \"" + rulePath + "\" --OutputFileFormat "+os.path.splitext(inputPath)[1]+" --Verbose" + '\"'
 
         print(command)
         process = Popen(command, shell = True)
